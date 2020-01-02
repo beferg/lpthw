19e5d629cdabffed4f18f6220a180d8f899277e2
Byron Ferguson <byron337@gmail.com>
2020-01-02 04:18:59 -0600

    Exercise 13 - Parameters, Unpacking, Variables

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# read the WYSS secsion for how to run this
+script, first, second, third, fourth = argv
+
+print("The script is called:", script)
+print("Your first varriable is:", first)
+print("Your second varriable is:", second)
+print("Your third varriable is:", third)
+
+print("Your name is", fourth)
